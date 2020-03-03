5688b1835885188b7ed7b1fc9fb31ded5c0a9044
HB <hbianan@gmail.com>
2020-03-03 13:34:30 -0500

    files added

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT = 8000;
+
+app.get('/', (request, response) => {
+    response.send(`
+        <h1>Status Code: ${response.statusCode}</h1>
+        <h2>Hello World</h2>
+    `)
+});
+
+app.listen(PORT, () => {
+    console.log(`App is listening on http://localhost:${PORT}`);
+})
