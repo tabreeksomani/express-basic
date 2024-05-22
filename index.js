588c62b1134083e1693f52c271dde32b353821ef
tsomani <tabreek.somani@gmail.com>
2024-05-22 09:28:46 -0700

    Added port visibility

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 8000;
 
 app.get('/', (request, response) => {
     response.send(`
-        <h1>Status Code: ${response.statusCode}</h1>
+        <h1>Status Code: ${response.statusCode} PORT ${PORT}</h1>
         <h2>Hello World</h2>
     `)
 });
